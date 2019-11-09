5193af77f3b2e71b8cca5f88475b1ede35d56055
tessssssssy <tessfabry@gmail.com>
2019-11-09 19:11:11 +1100

    initial commit

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,25 @@
-const caesar = function() {
+/*
+In cryptography, a Caesar cipher, also known as Caesar's cipher, 
+the shift cipher, Caesar's code or Caesar shift, is one of the 
+simplest and most widely known encryption techniques. It is a 
+type of substitution cipher in which each letter in the plaintext 
+is replaced by a letter some fixed number of positions down the alphabet. 
+For example, with a left shift of 3, D would be replaced by A, E would 
+become B, and so on. The method is named after Julius Caesar, who used 
+it in his private correspondence.
 
-}
+write a function that takes a string to be encoded and a shift factor 
+and then returns the encoded string:
+*/
+//takes in two params:
+//string - the word or phrase to be encoded
+//number - shift factor, how many places down the alphabet its shifted
 
-module.exports = caesar
+//retains capitalization
+//does not shift punctuation
+//continuous Z - A
+//works for negative numbers (shifts backwards)
+
+const caesar = function() {};
+
+module.exports = caesar;
